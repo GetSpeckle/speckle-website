2d0fcdce50e2f965b1e7c863c53d3a32e70f9477
hskang9 <hskang9@gmail.com>
2019-08-19 08:52:07 +0200

    Add image options

--- speckle-website/src/components/home.js
+++ speckle-website/src/components/home.js
@@ -16,7 +16,8 @@ import screen4 from "../images/Screen4.svg"
 import screen5 from "../images/Screen5.svg"
 
 const flickityOptions = {
-  initialIndex: 0
+  initialIndex: 0,
+  imagesLoaded: true
 }
 
 class Home extends React.Component {
